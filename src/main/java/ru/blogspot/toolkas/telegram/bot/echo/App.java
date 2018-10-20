f9d369b7708575c98ed807f2f18c21ee21de0013
tulkas <1@id-mt.ru>
2018-10-20 20:04:38 +0300

    init

---
+++ src/main/java/ru/blogspot/toolkas/telegram/bot/echo/App.java
@@ -0,0 +1,14 @@
+package ru.blogspot.toolkas.telegram.bot.echo;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+public class App {
+    public static void main(String[] args) throws TelegramApiRequestException {
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        botsApi.registerBot(new EchoBot());
+    }
+}
---
+++ src/main/java/ru/blogspot/toolkas/telegram/bot/echo/EchoBot.java
@@ -0,0 +1,56 @@
+package ru.blogspot.toolkas.telegram.bot.echo;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+/**
+ * Класс-обработчик поступающих к боту сообщений.
+ */
+public class EchoBot extends TelegramLongPollingBot {
+    /**
+     * Метод, который возвращает токен, выданный нам ботом @BotFather.
+     * @return токен
+     */
+    @Override
+    public String getBotToken() {
+        return "сюда вставьте токен от BotFather";
+    }
+
+    /**
+     * Метод-обработчик поступающих сообщений.
+     * @param update объект, содержащий информацию о входящем сообщении
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            //проверяем есть ли сообщение и текстовое ли оно
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                //Извлекаем объект входящего сообщения
+                Message inMessage = update.getMessage();
+                //Создаем исходящее сообщение
+                SendMessage outMessage = new SendMessage();
+                //Указываем в какой чат будем отправлять сообщение
+                //(в тот же чат, откуда пришло входящее сообщение)
+                outMessage.setChatId(inMessage.getChatId());
+                //Указываем текст сообщения
+                outMessage.setText(inMessage.getText());
+                //Отправляем сообщение
+                execute(outMessage);
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Метод, который возвращает имя пользователя бота.
+     * @return имя пользователя
+     */
+    @Override
+    public String getBotUsername() {
+        return "<сюда вставьте имя пользователя бота>";
+    }
+}
